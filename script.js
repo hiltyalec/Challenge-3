a066695aca1f5ab7d52065f83c9cdfd20ff51c0e
Alec Hilty <hiltyalec@gmail.com>
2024-02-22 16:38:21 -0500

    Fleshed out the variables and added functions Added logic and prompts to set criteria

--- script.js
+++ script.js
@@ -19,19 +19,39 @@ function generatePassword(){
         "Your password must meet the requirement of 8 to 128 characters, enter the desired number of characters in a numberic character. "
     );
 
-    
-    
-    return "Generated Password";
+    if (passLength < 8 || passLength > 128 || isNaN(passLength)){
+        alert("Your password did not meet the character requirement. Password must be between 8 and 128 characters. Please enter another number between 8 and 128.");
+        return;
+    } else {
+        var lettersUppercase = confirm("Click ok in order to generate uppercase letters in your password.");
+        var lettersLowercase = confirm("Click ok in order to generate lowercase letters in your password.");
+        var chooseNumbers = confirm("Click ok in order to generate numbers in your password.");
+        var chooseSymbols = confirm("Click ok in order to generate symbols in your password.");
+    }
+
+    if(lettersUppercase) {multiple += uppercaseLetters;}
+    if(lettersLowercase) {multiple += lowercaseLetters;}
+    if(chooseNumbers) {multiple += numbers;}
+    if (chooseSymbols) {multiple += symbols;}
+
+    for (x = 0; x < passLength; x++) {
+        
+        randomPass += multiple[Math.floor(Math.random() * multiple.length)]
+    }
+    return(randomPass);
 }
 
+    var passwordText = document.querySelector("#password");
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword(randomPass) {
+    if (password.length ===0){
+        return;
+    }
+ 
 
-  passwordText.value = password;
+  passwordText.value = randomPass;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function(){ writePassword(generatePassword())});
