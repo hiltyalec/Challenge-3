e5d6e74022467f7cbd21b6643a46a08732ebcd0e
Alec Hilty <hiltyalec@gmail.com>
2024-02-22 16:10:23 -0500

    Added some variables

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+    console.log("Button Click");
+
+    //prompt w password criteria
+    // length 8-128, character types: lowercase, uppercase, numberic, special characters.
+    // input should validate and 1 character type must be selected
+    var uppercaseLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    var lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm";
+    var numbers = "1234567890";
+    var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var multiple = "";
+    var randomPass = "";
+
+    var passLength = prompt(
+        "Your password must meet the requirement of 8 to 128 characters, enter the desired number of characters in a numberic character. "
+    );
+
+    
+    
+    return "Generated Password";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
